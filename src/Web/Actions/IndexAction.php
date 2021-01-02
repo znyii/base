abcf6b759203031d4fd08d7d441eb619489fa545
Vitaliy <theyamshikov@yandex.ru>
2021-01-02 21:45:55 +0600

    upd

--- src/Web/Actions/IndexAction.php
+++ src/Web/Actions/IndexAction.php
@@ -2,10 +2,13 @@
 
 namespace ZnYii\Base\Web\Actions;
 
+use ZnCore\Base\Legacy\Yii\Helpers\ArrayHelper;
+use ZnCore\Domain\Helpers\EntityHelper;
 use ZnCore\Domain\Helpers\QueryHelper;
 use Yii;
 use ZnCore\Domain\Interfaces\Entity\ValidateEntityInterface;
 use ZnCore\Domain\Interfaces\Service\ServiceDataProviderByFilterInterface;
+use ZnCore\Domain\Libs\Query;
 
 class IndexAction extends BaseAction
 {
@@ -36,6 +39,12 @@ class IndexAction extends BaseAction
         $query->addOrderBy($this->sort);
         if($this->service instanceof ServiceDataProviderByFilterInterface) {
             $filterModel = new $this->filterModel;
+            $whereParams = $query->getParam(Query::WHERE);
+            $filterAttributes = ArrayHelper::getValue($whereParams, 'filter');
+            $query->removeParam(Query::WHERE);
+            //dd($query);
+            EntityHelper::setAttributes($filterModel, $filterAttributes);
+            //dd($filterModel);
             $dataProvider = $this->service->getDataProviderByFilter($query, [], $filterModel);
         } else {
             $dataProvider = $this->service->getDataProvider($query);
